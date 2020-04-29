f849cc8402dd26ef72672d4a6409e0e58ac4fa9f
scrooger98 <wjpW9JWHXjXvnbL>
2020-04-29 09:59:23 +0200

    0.6
    0.6

--- myscripts.js
+++ myscripts.js
@@ -2,15 +2,15 @@ function changeImage()
 {
 var img = document.getElementById("image");
 if (img.src.includes("blau"))
-{img.src="rot.jpg";}
+{img.src="./images/rot.jpg";}
 else{
 if (img.src.includes("rot"))
-{img.src="schwarz.jpg";}
+{img.src="./images/schwarz.jpg";}
 else{
 if (img.src.includes("schwarz"))
-{img.src="grau.jpg";}
+{img.src="./images/grau.jpg";}
 else{
-{img.src="blau.jpg";}    
+{img.src="./images/blau.jpg";}    
 }
 }
 }
@@ -22,12 +22,12 @@ function changeImage1()
 var img = document.getElementById("image2");
 var img2 = document.getElementById("image3");
 if (img.src.includes("car1"))
-{img.src="car2.png"; img2.src="FelgenMercedes1.png";}
+{img.src="./images/car2.png"; img2.src="./images/FelgenMercedes1.png";}
 else{
 if (img.src.includes("car2"))
-{img.src="car3.png";img2.src="FelgenPorsche1.png";}
+{img.src="./images/car3.png";img2.src="./images/FelgenPorsche1.png";}
 else{
-{img.src="car1.png";img2.src="FelgenSmart1.png";}
+{img.src="./images/car1.png";img2.src="./images/FelgenSmart1.png";}
 }
 }
 return false;
@@ -39,16 +39,16 @@ var img = document.getElementById("image2");
 var img2 = document.getElementById("image3");
 if (img.src.includes("car1"))
 {if (img2.src.includes("FelgenSmart1"))
-{img2.src="FelgenSmart2.png";}else{img2.src="FelgenSmart1.png";}}
+{img2.src="./images/FelgenSmart2.png";}else{img2.src="./images/FelgenSmart1.png";}}
     
 else{
 if (img.src.includes("car2"))
 {if (img2.src.includes("FelgenMercedes1"))
-{img2.src="FelgenMercedes2.png";}else{img2.src="FelgenMercedes1.png";}}
+{img2.src="./images/FelgenMercedes2.png";}else{img2.src="./images/FelgenMercedes1.png";}}
     
 else{
 {if (img2.src.includes("Porsche1"))
-{img2.src="FelgenPorsche2.png";}else{img2.src="FelgenPorsche1.png";}}
+{img2.src="./images/FelgenPorsche2.png";}else{img2.src="./images/FelgenPorsche1.png";}}
 }
 }
 return false;
