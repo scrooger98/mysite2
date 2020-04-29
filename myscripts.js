2f33466a7200bca71660f20038d0cff7950161e8
scrooger98 <wjpW9JWHXjXvnbL>
2020-04-29 09:16:19 +0200

    0.5

--- myscripts.js
+++ myscripts.js
@@ -20,10 +20,35 @@ return false;
 function changeImage1()
 {
 var img = document.getElementById("image2");
-if (img.src.includes("1"))
-{img.src="car2.png";}
-else{img.src="car1.png";
-
+var img2 = document.getElementById("image3");
+if (img.src.includes("car1"))
+{img.src="car2.png"; img2.src="FelgenMercedes1.png";}
+else{
+if (img.src.includes("car2"))
+{img.src="car3.png";img2.src="FelgenPorsche1.png";}
+else{
+{img.src="car1.png";img2.src="FelgenSmart1.png";}
+}
+}
+return false;
+}
+function changeImage2()
+{
+var img = document.getElementById("image2");
+var img2 = document.getElementById("image3");
+if (img.src.includes("car1"))
+{if (img2.src.includes("FelgenSmart1"))
+{img2.src="FelgenSmart2.png";}else{img2.src="FelgenSmart1.png";}}
+    
+else{
+if (img.src.includes("car2"))
+{if (img2.src.includes("FelgenMercedes1"))
+{img2.src="FelgenMercedes2.png";}else{img2.src="FelgenMercedes1.png";}}
+    
+else{
+{if (img2.src.includes("Porsche1"))
+{img2.src="FelgenPorsche2.png";}else{img2.src="FelgenPorsche1.png";}}
+}
 }
 return false;
 }
