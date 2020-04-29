da68dbd31da538933bbe86e4e806a5a5a03eb69d
scrooger98 <wjpW9JWHXjXvnbL>
2020-04-29 15:26:12 +0200

    0.96

--- connect.php
+++ connect.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title> PHP </title>
+	</head>
+	<body>
+
+	
 <?php
 $connection = pg_connect ("host=ec2-79-125-26-232.eu-west-1.compute.amazonaws.com dbname=da5ra37bdrqbo5 user=cloqhtjxzygxxn password=ed2f479dd21985a83d9d022d7c986bc77bac577343e2eafd4ad51b8051f6c487");
 if($connection) {
@@ -8,3 +18,10 @@ echo 'there has been an error connecting';
 } 
 
 ?>
+        
+        	
+	
+	
+	
+	</body>
+</html>
