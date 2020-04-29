86d88867e39177176a8f79c3b2e9c331fcd7e94d
scrooger98 <wjpW9JWHXjXvnbL>
2020-04-29 08:11:03 +0200

    0.4

--- dbconnect.php
+++ dbconnect.php
@@ -1,3 +1,3 @@
 <?php
-$db = mysqli_connect("localhost", "cloqhtjxzygxxn", "ed2f479dd21985a83d9d022d7c986bc77bac577343e2eafd4ad51b8051f6c487", "da5ra37bdrqbo5");
+$db = mysqli_connect("ec2-79-125-26-232.eu-west-1.compute.amazonaws.com", "cloqhtjxzygxxn", "ed2f479dd21985a83d9d022d7c986bc77bac577343e2eafd4ad51b8051f6c487", "da5ra37bdrqbo5");
 ?>
