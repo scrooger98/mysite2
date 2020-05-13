0eb0fc39f617f1f6f6d20bbd12038a7aeab2c11b
scrooger98 <wjpW9JWHXjXvnbL>
2020-05-13 09:15:18 +0200

    superduperseitevonnick

--- navbar.php
+++ navbar.php
@@ -1,14 +1,14 @@
 <div class="navbar">
   <a href="./index.php">Home</a>
-  <a href="./konfig.html">Konfigurator</a>
+  <a href="./konfig.php">Konfigurator</a>
   <div class="dropdown">
     <button class="dropbtn">Infos
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
       <a href="./wallpaper.php">Wallpaper</a>
-      <a href="./datenschutz.html">Datenschutz</a>
-      <a href="./impressum.html">Impressum</a>
+      <a href="./datenschutz.php">Datenschutz</a>
+      <a href="./impressum.php">Impressum</a>
     </div>
   </div>
 </div>
