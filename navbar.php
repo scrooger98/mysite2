a9ca07b7531e302941da57e354eba093036044ab
scrooger98 <wjpW9JWHXjXvnbL>
2020-05-13 09:00:33 +0200

    testtesttest

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<div class="navbar">
+  <a href="./index.php">Home</a>
+  <a href="./konfig.html">Konfigurator</a>
+  <div class="dropdown">
+    <button class="dropbtn">Infos
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="./wallpaper.php">Wallpaper</a>
+      <a href="./datenschutz.html">Datenschutz</a>
+      <a href="./impressum.html">Impressum</a>
+    </div>
+  </div>
+</div>
